ab03ae26d01eee63a8574f9bf29d3824102dc535
RezaShahbaz <r.shahbaz.rs73@gmail.com>
2024-12-13 13:13:04 +0330

    refactor: Change Primitive Types To Objects

--- sfo/src/main/java/com/graphhopper/sfo/rgeocode/SnapRequest.java
+++ sfo/src/main/java/com/graphhopper/sfo/rgeocode/SnapRequest.java
@@ -3,25 +3,25 @@ package com.graphhopper.sfo.rgeocode;
 import static com.graphhopper.util.Helper.round;
 
 public class SnapRequest {
-    private double lat;
-    private double lon;
+    private Double lat;
+    private Double lon;
 
     public SnapRequest() {
     }
 
-    public double getLat() {
+    public Double getLat() {
         return lat;
     }
 
-    public void setLat(double lat) {
+    public void setLat(Double lat) {
         this.lat = round(lat, 7);
     }
 
-    public double getLon() {
+    public Double getLon() {
         return lon;
     }
 
-    public void setLon(double lon) {
+    public void setLon(Double lon) {
         this.lon = round(lon, 7);
     }
 }
