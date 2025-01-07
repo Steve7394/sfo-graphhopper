bd20f9e746c8001e2af687b7edf5ffe34f6d695c
r.shahbaz <r.shahbaz.rs73@gmail.com>
2025-01-07 12:46:32 +0330

    feat: Add All Administrative Polygon Properties To Reverse Geocode Endpoint

--- sfo/src/main/java/com/graphhopper/sfo/rgeocode/dto/SnapResponse.java
+++ sfo/src/main/java/com/graphhopper/sfo/rgeocode/dto/SnapResponse.java
@@ -1,17 +1,135 @@
 package com.graphhopper.sfo.rgeocode.dto;
 
 public class SnapResponse {
-    private String country;
-    private String province;
-    private String city;
     private String street;
     private String streetType;
-    private Integer streetOsmId;
-    private Integer provinceOsmId;
-    private Integer cityOsmId;
     private Double streetMaxSpeed;
+    private Integer streetOsmId;
+
+
+    private String country;
+
+    private String province;
+    private Integer provinceOsmId;
+
+    private String city;
+    private Integer cityOsmId;
+
+    private String county;
+    private Integer countyOsmId;
+
+    private String district;
+    private Integer districtOsmId;
+
+    private String village;
+    private Integer villageOsmId;
+
+    private String suburb;
+    private Integer suburbOsmId;
+
+    private String subarea;
+    private Integer subareaOsmId;
+
+    private String neighbourhood;
+    private Integer neighbourhoodOsmId;
 
     public SnapResponse(){}
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public Integer getCountyOsmId() {
+        return countyOsmId;
+    }
+
+    public void setCountyOsmId(Integer countyOsmId) {
+        this.countyOsmId = countyOsmId;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Integer getDistrictOsmId() {
+        return districtOsmId;
+    }
+
+    public void setDistrictOsmId(Integer districtOsmId) {
+        this.districtOsmId = districtOsmId;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+    public Integer getVillageOsmId() {
+        return villageOsmId;
+    }
+
+    public void setVillageOsmId(Integer villageOsmId) {
+        this.villageOsmId = villageOsmId;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public Integer getSuburbOsmId() {
+        return suburbOsmId;
+    }
+
+    public void setSuburbOsmId(Integer suburbOsmId) {
+        this.suburbOsmId = suburbOsmId;
+    }
+
+    public String getSubarea() {
+        return subarea;
+    }
+
+    public void setSubarea(String subarea) {
+        this.subarea = subarea;
+    }
+
+    public Integer getSubareaOsmId() {
+        return subareaOsmId;
+    }
+
+    public void setSubareaOsmId(Integer subareaOsmId) {
+        this.subareaOsmId = subareaOsmId;
+    }
+
+    public String getNeighbourhood() {
+        return neighbourhood;
+    }
+
+    public void setNeighbourhood(String neighbourhood) {
+        this.neighbourhood = neighbourhood;
+    }
+
+    public Integer getNeighbourhoodOsmId() {
+        return neighbourhoodOsmId;
+    }
+
+    public void setNeighbourhoodOsmId(Integer neighbourhoodOsmId) {
+        this.neighbourhoodOsmId = neighbourhoodOsmId;
+    }
 
     public String getCountry() {
         return country;
--- sfo/src/main/java/com/graphhopper/sfo/rgeocode/service/ReverseGeocodeService.java
+++ sfo/src/main/java/com/graphhopper/sfo/rgeocode/service/ReverseGeocodeService.java
@@ -3,10 +3,7 @@ package com.graphhopper.sfo.rgeocode.service;
 import com.graphhopper.routing.ev.*;
 import com.graphhopper.routing.util.EdgeFilter;
 import com.graphhopper.routing.util.EncodingManager;
-import com.graphhopper.routing.util.parsers.CityNameParser;
-import com.graphhopper.routing.util.parsers.CityOsmIdParser;
-import com.graphhopper.routing.util.parsers.ProvinceNameParser;
-import com.graphhopper.routing.util.parsers.ProvinceOsmIdParser;
+import com.graphhopper.routing.util.parsers.sfo.*;
 import com.graphhopper.sfo.rgeocode.dto.SnapRequest;
 import com.graphhopper.sfo.rgeocode.dto.SnapResponse;
 import com.graphhopper.storage.index.LocationIndex;
@@ -61,11 +58,32 @@ public class ReverseGeocodeService {
         response.setStreetType(edge.get(encodingManager.getEnumEncodedValue(RoadClass.KEY, RoadClass.class)).name());
         response.setStreetWayId(edge.get(encodingManager.getIntEncodedValue(OSMWayID.KEY)));
         response.setStreetMaxSpeed(edge.get(encodingManager.getDecimalEncodedValue(MaxSpeed.KEY)));
+
         response.setCountry(edge.get(encodingManager.getEnumEncodedValue(Country.KEY, Country.class)).getCountryName());
+
         response.setProvince(edge.get(encodingManager.getStringEncodedValue(ProvinceNameParser.KEY)));
         response.setProvinceOsmId(edge.get(encodingManager.getIntEncodedValue(ProvinceOsmIdParser.KEY)));
+
         response.setCity(edge.get(encodingManager.getStringEncodedValue(CityNameParser.KEY)));
         response.setCityOsmId(edge.get(encodingManager.getIntEncodedValue(CityOsmIdParser.KEY)));
+
+        response.setCounty(edge.get(encodingManager.getStringEncodedValue(CountyNameParser.KEY)));
+        response.setCountyOsmId(edge.get(encodingManager.getIntEncodedValue(CountyOsmIdParser.KEY)));
+
+        response.setDistrict(edge.get(encodingManager.getStringEncodedValue(DistrictNameParser.KEY)));
+        response.setDistrictOsmId(edge.get(encodingManager.getIntEncodedValue(DistrictOsmIdParser.KEY)));
+
+        response.setVillage(edge.get(encodingManager.getStringEncodedValue(VillageNameParser.KEY)));
+        response.setVillageOsmId(edge.get(encodingManager.getIntEncodedValue(VillageOsmIdParser.KEY)));
+
+        response.setSuburb(edge.get(encodingManager.getStringEncodedValue(SuburbNameParser.KEY)));
+        response.setSuburbOsmId(edge.get(encodingManager.getIntEncodedValue(SuburbOsmIdParser.KEY)));
+
+        response.setSubarea(edge.get(encodingManager.getStringEncodedValue(SubareaNameParser.KEY)));
+        response.setSubareaOsmId(edge.get(encodingManager.getIntEncodedValue(SubareaOsmIdParser.KEY)));
+
+        response.setNeighbourhood(edge.get(encodingManager.getStringEncodedValue(NeighbourhoodNameParser.KEY)));
+        response.setNeighbourhoodOsmId(edge.get(encodingManager.getIntEncodedValue(NeighbourhoodOsmIdParser.KEY)));
         return response;
     }
 }
